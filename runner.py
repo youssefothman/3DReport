7927cba8c0cd671abd9aae480b18ddb57886d2dc
youssef-othman <125914257+youssef-othman@users.noreply.github.com>
2025-06-20 12:03:39 -0400

    removed python dependency

--- runner.py
+++ runner.py
@@ -1,10 +1,6 @@
-import os
-import subprocess
+import report_builder
 
-# Inject API keys into environment
-env = os.environ.copy()
-env["WHOIS_API_KEY"] = "at_w5FxDIhFFkwqQdYdGs2tiszELl2R0"
-env["THREATINT_API_KEY"] = "at_wjKFvkN7iUpqywnneyMtRPquQ9TTw"
+WHOIS_API_KEY = "at_w5FxDIhFFkwqQdYdGs2tiszELl2R0"
+THREATINT_API_KEY = "at_wjKFvkN7iUpqywnneyMtRPquQ9TTw"
 
-# Run their editable script
-subprocess.run(["python", "3D_report_builder.py"], env=env)
+report_builder.main(WHOIS_API_KEY, THREATINT_API_KEY)
